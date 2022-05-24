3b95ef684c5157eb5a62988b3966e0285240ca99
Muzaffar <muzaffar.umurzakov93@gmail.com>
2022-05-24 13:10:27 +0500

    php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+//$car = array(2, "Nexia", 4, "Malibu", '#');
+//$subject = ['calculus', 'path' , 'map'];
+
+//print_r($car);
+
+//$car[0] = "cobalt";
+
+//print_r($car);
+
+//$car[] = "ferrari";
+
+//print_r($car);
+
+//$color = array('r' => "red", 'b' => "blue", 'g' => "grey", 'y' => "yellow");
+
+//if (in_array("black", $color)) {
+//    echo "Found";
+//}
+//else {
+//    echo "Not found";
+//}
+
+//$number = array_fill(23, 34, "p");
+
+//print_r($number) 
+
+$color1 = ["white", "blue", "red"];
+
+$color2 = ["black", "yellow", "green"];
+
+$colors = array_merge($color1, $color2);
+
+print_r($colors);
+
+?>
